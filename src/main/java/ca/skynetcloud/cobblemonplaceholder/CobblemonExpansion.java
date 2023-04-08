464d3a4806102769663d20f037d22bb2f239b614
Rakise <74441654+Rakise@users.noreply.github.com>
2023-04-08 18:46:38 +0000

    Get pokemon class

--- src/main/java/ca/skynetcloud/cobblemonplaceholder/CobblemonExpansion.java
+++ src/main/java/ca/skynetcloud/cobblemonplaceholder/CobblemonExpansion.java
@@ -114,6 +114,7 @@ public class CobblemonExpansion extends PlaceholderExpansion {
                 new SpeciesTotalCountParser(),
 
                 new DexNumberParser(),
+                new PokemonParser(),
                 new AbilityParser(),
                 new ExperienceParser(),
 
--- src/main/java/ca/skynetcloud/cobblemonplaceholder/impl/party/PokemonParser.java
+++ src/main/java/ca/skynetcloud/cobblemonplaceholder/impl/party/PokemonParser.java
@@ -1,1 +1,30 @@
-a
+
+package ca.skynetcloud.cobblemonplaceholder.impl.PokemonParser;
+
+import ca.skynetcloud.cobblemonplaceholder.api.Parser;
+import ca.skynetcloud.cobblemonplaceholder.CobblemonExpansion;
+import ca.skynetcloud.cobblemonplaceholder.impl.PartyParser;
+import ca.skynetcloud.cobblemonplaceholder.util.text.Text;
+import com.cobblemon.mod.common.pokemon.Pokemon;
+import org.bukkit.entity.Player;
+
+public class PokemonGet implements Parser {
+
+    @Override
+    public String getID() {
+        return "party_" + this.slot + "_pokemon";
+    }
+
+    @Override
+    public void register() {
+        for (int i = 1; i <= 6; ++i) {
+            CobblemonExpansion.registerParser(new PokemonParser(i));
+        }
+    }
+
+    @Override
+    public static Pokemon parse(final Player player, final String[] args) throws NoPokemonStoreException {
+        int slot = Integer.parseInt(args[0]);
+        return getPokemon(player, slot);
+    }
+}
