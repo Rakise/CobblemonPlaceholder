9436b3a765cff0eaedac1b92b2b25698c7c3f0b4
Rakise <74441654+Rakise@users.noreply.github.com>
2023-04-18 03:11:49 -0300

    Create BattleVictoryEvent.java

---
+++ src/main/java/ca/skynetcloud/cobblemonplaceholder/BattleVictoryEvent.java
@@ -0,0 +1,15 @@
+import org.bukkit.event.Event;
+import java.util.List;
+
+public class BattleVictoryEvent extends Event {
+
+    private final List<BattleActor> winners;
+
+    public BattleVictoryEvent(List<BattleActor> winners) {
+        this.winners = winners;
+    }
+
+    public List<BattleActor> getWinners() {
+        return winners;
+    }
+}
