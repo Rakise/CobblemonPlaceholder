a6073ed005ad063a5d842caa604c3ea7e65083a2
Rakise <74441654+Rakise@users.noreply.github.com>
2023-04-18 01:30:57 -0300

    Update Cobblemonplaceholder.java

--- src/main/java/ca/skynetcloud/cobblemonplaceholder/Cobblemonplaceholder.java
+++ src/main/java/ca/skynetcloud/cobblemonplaceholder/Cobblemonplaceholder.java
@@ -1,6 +1,27 @@
 package ca.skynetcloud.cobblemonplaceholder;
 
+import com.cobblemon.mod.common.Cobblemon;
 import org.bukkit.Bukkit;
+import com.cobblemon.mod.common.api.pokemon.evolution.PreEvolution;
+import com.cobblemon.mod.common.api.events.CobblemonEvents;
+import com.cobblemon.mod.common.api.Priority;
+import com.cobblemon.mod.common.api.events.battles.BattleVictoryEvent;
+import java.util.List;
+import com.cobblemon.mod.common.pokemon.evolution.CobblemonLazyPreEvolution;
+import com.cobblemon.mod.common.api.pokemon.PokemonSpecies;
+import com.cobblemon.mod.common.api.battles.model.PokemonBattle;
+import com.cobblemon.mod.common.api.battles.model.actor.BattleActor;
+import com.cobblemon.mod.common.pokemon.FormData;
+import com.cobblemon.mod.common.command.argument.PokemonPropertiesArgumentType;
+import com.cobblemon.mod.common.pokemon.Species;
+import com.cobblemon.mod.common.api.pokemon.PokemonProperties;
+import com.cobblemon.mod.common.api.storage.party.PlayerPartyStore;
+import ca.skynetcloud.cobblemonplaceholder.CobblemonExpansion;
+import com.cobblemon.mod.common.api.storage.NoPokemonStoreException;
+import com.cobblemon.mod.common.pokemon.Pokemon;
+import org.bukkit.entity.Player;
+
+import java.util.UUID;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
