9ba250f85ea092aaa04bca43f17b623518195f6c
Rakise <74441654+Rakise@users.noreply.github.com>
2023-04-18 03:36:46 -0300

    Update VictoryEvent.java

--- src/main/java/ca/skynetcloud/cobblemonplaceholder/VictoryEvent.java
+++ src/main/java/ca/skynetcloud/cobblemonplaceholder/VictoryEvent.java
@@ -1,9 +1,10 @@
 package ca.skynetcloud.cobblemonplaceholder;
 
 import ca.skynetcloud.cobblemonplaceholder.Cobblemonplaceholder;
+import org.bukkit.event.HandlerList;
+
 import org.bukkit.event.Event;
 import java.util.List;
-import java.util.Collections;
 import com.cobblemon.mod.common.api.battles.model.actor.BattleActor;
 
 public class VictoryEvent extends Event {
@@ -16,7 +17,7 @@ public class VictoryEvent extends Event {
     }
 
     public List<BattleActor> getWinners() {
-        return Collections.singletonList(winners.get(0).getName());
+        return winners.get(0).getName();
     }
     
     @Override
