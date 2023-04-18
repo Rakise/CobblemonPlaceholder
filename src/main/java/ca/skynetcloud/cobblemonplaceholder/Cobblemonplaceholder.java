04709a6cce25af073669154ca82edfd07cc18a53
Rakise <74441654+Rakise@users.noreply.github.com>
2023-04-18 01:49:31 -0300

    Update Cobblemonplaceholder.java

--- src/main/java/ca/skynetcloud/cobblemonplaceholder/Cobblemonplaceholder.java
+++ src/main/java/ca/skynetcloud/cobblemonplaceholder/Cobblemonplaceholder.java
@@ -30,7 +30,7 @@ public class Cobblemonplaceholder extends JavaPlugin {
     @Override
     public void onEnable() {
         System.out.println("teste");
-        CobblemonEvents.INSTANCE.BATTLE_VICTORY.subscribe(Priority.NORMAL, winevent -> {
+        CobblemonEvents.BATTLE_VICTORY.subscribe(Priority.NORMAL, winevent -> {
             List<BattleActor> winners = winevent.getWinners();
             System.out.println("Sending message to console...");
             Bukkit.getConsoleSender().sendMessage("winner: " + winners.get(0).getName());
