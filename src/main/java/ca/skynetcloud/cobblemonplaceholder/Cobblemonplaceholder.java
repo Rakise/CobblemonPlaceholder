8ca953c3101424555d9cbba71c1d0c36e3bec4a6
Rakise <74441654+Rakise@users.noreply.github.com>
2023-04-18 01:28:33 -0300

    Update Cobblemonplaceholder.java

--- src/main/java/ca/skynetcloud/cobblemonplaceholder/Cobblemonplaceholder.java
+++ src/main/java/ca/skynetcloud/cobblemonplaceholder/Cobblemonplaceholder.java
@@ -4,11 +4,17 @@ import org.bukkit.Bukkit;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
-
 public class Cobblemonplaceholder extends JavaPlugin {
 
     @Override
     public void onEnable() {
+        Bukkit.getConsoleSender().sendMessage("inicializando...");
+        CobblemonEvents.INSTANCE.BATTLE_VICTORY.subscribe(Priority.NORMAL, winevent -> {
+            List<BattleActor> winners = winevent.getWinners();
+            System.out.println("Sending message to console...");
+            Bukkit.getConsoleSender().sendMessage("winner: " + winners.get(0).getName());
+            return null;
+        });
         if (Bukkit.getPluginManager().getPlugin("PlaceholderAPI") == null) {
             System.out.println("PlaceholderAPI not found, disabling CobblemonPlaceholder.");
             this.getServer().getPluginManager().disablePlugin((Plugin)this);
